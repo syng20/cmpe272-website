47f71e2ba3db0d3667c034cfd0ee19f175f98ade
syng20 <samyng20@gmail.com>
2025-11-13 16:34:05 -0800

    test curl pt 2

--- list_of_all_users.php
+++ list_of_all_users.php
@@ -13,12 +13,17 @@
         <div class="userssection"> 
             <p><strong>Source:</strong> williamgky.online</p>
             <?php
-                curl -H "Accept: application/json" -X GET http://williamgky.online/userlist.php -o -temp.json
-                $json = file_get_contents('temp.json'); 
-                if ($json === false) {
+                $ch = curl_init(); 
+                curl_setopt($ch, CURLOPT_URL, "http://williamgky.online/userlist.php");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']);
+                // curl http://williamgky.online/userlist.php -H "Accept: application/json"
+                $response = curl_exec($ch); 
+                if (curl_errno($ch)) {
                     die('Error reading the JSON file');
                 }
-                $json_data = json_decode($json, true); 
+                curl_close($ch);
+                $json_data = json_decode($response, true); 
                 if ($json_data === null) {
                     die('Error decoding the JSON file');
                 }
@@ -41,7 +46,7 @@
 
     
     <footer>
-        <p>Ver 2.5.15</p>
+        <p>Ver 2.5.16</p>
     </footer>
 
     <!-- http://williamgky.online/userlist.php -->
